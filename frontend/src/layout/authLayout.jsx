29a9e9e47a4e68bbf42945a5ccd4688c0fb5268b
KoderGoq <FernandoBussines10@gmail.com>
2023-05-09 10:43:26 -0600

    Forms Created

--- frontend/src/layout/authLayout.jsx
+++ frontend/src/layout/authLayout.jsx
@@ -3,8 +3,11 @@ import { Outlet } from 'react-router-dom';
 const AuthLayout = () => {
     return (
         <>
-            <h1>Administrador de Pacientes de Veterianaria</h1>
-            <Outlet />
+
+            <main className="container mx-auto md:grid md:grid-cols-2 mt-8 gap-12 p-5 items-center">
+                <Outlet />
+            </main>
+
         </>
     )
 }
--- frontend/src/paginas/RegistrarCuenta.jsx
+++ frontend/src/paginas/RegistrarCuenta.jsx
@@ -1,9 +1,53 @@
+import { Link } from "react-router-dom";
 
 const RegistrarCuenta = () => {
     return (
         <>
+            <div>
+                <h1 className="text-indigo-600 font-black text-6xl">Crea tu cuenta y Administra tus
+                    <span className="text-black"> Pacientes</span>
+                </h1>
+            </div>
 
-            <h1>Desde RegistrarCuenta</h1>
+            <div className='mt-20 md:mt-5 shadow-lg px-3 py-5 rounded-xl bg-white'>
+                <form>
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Tu nombre
+                        </label>
+                        <input type="text" placeholder="Tu nombre" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+                    </div>
+
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Email
+                        </label>
+                        <input type="email" placeholder="Registra tu email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+                    </div>
+
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Password
+                        </label>
+                        <input type="password" placeholder="Tu password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+                    </div>
+
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Confirmar Password
+                        </label>
+                        <input type="password" placeholder="Confirma tu password" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+                    </div>
+
+                    <input type="submit" value="Crear Cuenta" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto" />
+                </form>
+
+                <nav className='mt-10 lg:flex lg:justify-between'>
+                    <Link className='block text-center my-5 text-gray-500' to="/">¿Ya tienes una cuenta? Inicia Sesion</Link>
+                    <Link className='block text-center my-5 text-gray-500' to="/olvide-password">Olvide Password</Link>
+                </nav>
+            </div>
+
 
         </>
     )

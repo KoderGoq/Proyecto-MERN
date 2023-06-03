87e9fa3d4197c3174f72e3f3cc9fcfe98151f980
KoderGoq <FernandoBussines10@gmail.com>
2023-06-03 16:40:40 -0600

    Autentication login users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,24 @@ import ConfirmarCuenta from './paginas/ConfirmarCuenta';
 import OlvidePassword from './paginas/OlvidePassword';
 import RegistrarCuenta from './paginas/RegistrarCuenta';
 import { NuevoPassoword } from './paginas/NuevoPassoword';
+import { AuthProvider } from './context/AuthProvider';
 
 function App() {
 
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<AuthLayout />}>
-					<Route index element={<Login />} />
-					<Route path='registrar' element={<RegistrarCuenta />} />
-					<Route path='olvide-password' element={<OlvidePassword />} />
-					<Route path='olvide-password/:token' element={<NuevoPassoword />} />
-					<Route path='confirmar/:id' element={<ConfirmarCuenta />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+			<AuthProvider>
+				<Routes>
+					<Route path='/' element={<AuthLayout />}>
+						<Route index element={<Login />} />
+						<Route path='registrar' element={<RegistrarCuenta />} />
+						<Route path='olvide-password' element={<OlvidePassword />} />
+						<Route path='olvide-password/:token' element={<NuevoPassoword />} />
+						<Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+					</Route>
+				</Routes>
+			</AuthProvider>
+		</BrowserRouter >
 	)
 }
 

3dc3de639d60c4d797e7a672c3981fa01fd5ee37
KoderGoq <FernandoBussines10@gmail.com>
2023-06-04 22:22:40 -0600

    change passowrds of veterinarios

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'; // Importamos Express para cofigurar el router
-import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword, actualizarPerfil } from '../controllers/veterinarioController.js'; // Controlador del router
+import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword, actualizarPerfil, actualizarPassword } from '../controllers/veterinarioController.js'; // Controlador del router
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router(); // Llamamos el Router
@@ -14,5 +14,6 @@ router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
 // Area privada
 router.get('/perfil', authMiddleware, perfil); // Otra para los perfiles del veterinario
 router.put('/perfil/:id', authMiddleware, actualizarPerfil);
+router.put('/actualizar-password', authMiddleware, actualizarPassword);
 
 export default router;
